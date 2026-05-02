b417693d29fd5310ee261b045271e2da0dcd7503
renasyach <117002888+renasyach@users.noreply.github.com>
2026-05-02 16:55:35 +0700

    PLP-8 Mengganti favicon, revisi halaman profil

--- palapa/resources/views/notifikasi/index.blade.php
+++ palapa/resources/views/notifikasi/index.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
     <title>Notifikasi - PALAPA</title>
     <!-- Import icon phosphor yang digunakan di sidebar Anda -->
     <script src="https://unpkg.com/@phosphor-icons/web"></script>
